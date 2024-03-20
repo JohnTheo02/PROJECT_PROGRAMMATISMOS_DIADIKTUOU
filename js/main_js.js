dbe9004b9507a03f350d92edbb123ce29076e4d4
JohnTheo02 <up1083831@upnet.gr>
2024-03-20 20:55:23 +0200

    seventh commit

--- js/main_js.js
+++ js/main_js.js
@@ -1,3 +1,3 @@
 document.getElementById("first_button").addEventListener("click",function(){
-    window.open("school_selection.html","_blank");
+    window.location.href = "school_selection.html";
 });
